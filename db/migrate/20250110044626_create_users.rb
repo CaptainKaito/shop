5bbdcc7e5642beb809534eb4b7f775191a364c71
CaptainKaito <kaichi557@gmail.com>
2025-01-10 12:38:51 +0700

    up18

--- db/migrate/20250110044626_create_users.rb
+++ db/migrate/20250110044626_create_users.rb
@@ -8,9 +8,20 @@ class CreateUsers < ActiveRecord::Migration[8.0]
       t.timestamps
     end
 
+    # Thêm cột email_address nếu nó chưa tồn tại
+    unless column_exists?(:users, :email_address)
+      add_column :users, :email_address, :string, null: false
+    end
+
+    # Thêm cột password_digest nếu nó chưa tồn tại
+    unless column_exists?(:users, :password_digest)
+      add_column :users, :password_digest, :string, null: false
+    end
+
     # Thêm index cho cột email_address nếu chưa có
     unless index_exists?(:users, :email_address)
       add_index :users, :email_address, unique: true
     end
   end
 end
+
