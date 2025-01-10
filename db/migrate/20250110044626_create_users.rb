e4c479644dbd821498ff126ecba4e6fddab3e807
CaptainKaito <kaichi557@gmail.com>
2025-01-10 12:34:03 +0700

    up18

--- db/migrate/20250110044626_create_users.rb
+++ db/migrate/20250110044626_create_users.rb
@@ -1,11 +1,16 @@
-#class CreateUsers < ActiveRecord::Migration[8.0]
-  #def change
-    #create_table :users do |t|
-      #t.string :email_address, null: false
-      #t.string :password_digest, null: false
+class CreateUsers < ActiveRecord::Migration[8.0]
+  def change
+    # Tạo bảng users nếu chưa tồn tại
+    create_table :users, if_not_exists: true do |t|
+      t.string :email_address, null: false
+      t.string :password_digest, null: false
 
-     # t.timestamps
-   # end
-   # add_index :users, :email_address, unique: true
-  #end
-#end
+      t.timestamps
+    end
+
+    # Thêm index cho cột email_address nếu chưa có
+    unless index_exists?(:users, :email_address)
+      add_index :users, :email_address, unique: true
+    end
+  end
+end
--- db/migrate/20250110044639_create_sessions.rb
+++ db/migrate/20250110044639_create_sessions.rb
@@ -1,11 +1,21 @@
-#class CreateSessions < ActiveRecord::Migration[8.0]
-  #def change
-   # create_table :sessions do |t|
-     # t.references :user, null: false, foreign_key: true
-      #t.string :ip_address
-      #t.string :user_agent
+class CreateSessions < ActiveRecord::Migration[8.0]
+  def change
+    # Tạo bảng nếu chưa tồn tại
+    create_table :sessions, if_not_exists: true do |t|
+      t.references :user, null: false, foreign_key: true
+      t.string :ip_address
+      t.string :user_agent
 
-      #t.timestamps
-    #end
- # end
-#end
+      t.timestamps
+    end
+
+    # Nếu bảng sessions đã tồn tại, thêm cột (nếu chưa có)
+    unless column_exists?(:sessions, :ip_address)
+      add_column :sessions, :ip_address, :string
+    end
+
+    unless column_exists?(:sessions, :user_agent)
+      add_column :sessions, :user_agent, :string
+    end
+  end
+end
