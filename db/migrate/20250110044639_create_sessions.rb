74097512436a2af7423d5f5005bbc9cab4f3a4d0
CaptainKaito <kaichi557@gmail.com>
2025-01-10 14:35:57 +0700

    up24

--- db/migrate/20250110044639_create_sessions.rb
+++ db/migrate/20250110044639_create_sessions.rb
@@ -2,7 +2,7 @@ class CreateSessions < ActiveRecord::Migration[8.0]
   def change
     # Tạo bảng nếu chưa tồn tại
     create_table :sessions, if_not_exists: true do |t|
-      t.references :user, null: false, foreign_key: true
+      t.references :user, null: false, foreign_key: false
       t.string :ip_address
       t.string :user_agent
 
